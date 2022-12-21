5b1a1bfdccb7cee15e7b5489f007a55fb51aa58a
Clio Brichaut <clio.brichaut@gmail.com>
2022-12-21 19:21:03 +0100

    Cleanup: removing unused code.

--- app/app/Http/Controllers/API/V1/ProductController.php
+++ app/app/Http/Controllers/API/V1/ProductController.php
@@ -89,7 +89,8 @@ class ProductController extends APIController
 		if (
 			is_array($request->query()) &&
 			($query = (new ProductQueryFilter())->convertToQuery($request->query())) !== []
-		) {
+		)
+		{
 			$losses->where($query);
 		}
 
@@ -100,7 +101,8 @@ class ProductController extends APIController
 				($json = json_encode($loss)) === false ||
 				($data = json_decode($json, true)) === false ||
 				(fputcsv($handle, $data)) === false // @phpstan-ignore-line
-			) {
+			)
+			{
 				return $this->returnErrorResponse(500, ['Could not write in file']);
 			}
 		}
@@ -109,80 +111,4 @@ class ProductController extends APIController
 
 		return response()->download($filename, $filename, ['content-type' => 'text/csv'])->deleteFileAfterSend();
 	}
-
-	// /**
-	//  * Display a listing of the resource.
-	//  *
-	//  * @return \Illuminate\Http\Response
-	//  */
-	// public function index()
-	// {
-	// 	//
-	// }
-
-	// /**
-	//  * Show the form for creating a new resource.
-	//  *
-	//  * @return \Illuminate\Http\Response
-	//  */
-	// public function create()
-	// {
-	// 	//
-	// }
-
-	// /**
-	//  * Store a newly created resource in storage.
-	//  *
-	//  * @param  \App\Http\Requests\StoreProductRequest  $request
-	//  * @return \Illuminate\Http\Response
-	//  */
-	// public function store(StoreProductRequest $request)
-	// {
-	// 	//
-	// }
-
-	// /**
-	//  * Display the specified resource.
-	//  *
-	//  * @param  \App\Models\Product  $product
-	//  * @return \Illuminate\Http\Response
-	//  */
-	// public function show(Product $product)
-	// {
-	// 	//
-	// }
-
-	// /**
-	//  * Show the form for editing the specified resource.
-	//  *
-	//  * @param  \App\Models\Product  $product
-	//  * @return \Illuminate\Http\Response
-	//  */
-	// public function edit(Product $product)
-	// {
-	// 	//
-	// }
-
-	// /**
-	//  * Update the specified resource in storage.
-	//  *
-	//  * @param  \App\Http\Requests\UpdateProductRequest  $request
-	//  * @param  \App\Models\Product  $product
-	//  * @return \Illuminate\Http\Response
-	//  */
-	// public function update(UpdateProductRequest $request, Product $product)
-	// {
-	// 	//
-	// }
-
-	// /**
-	//  * Remove the specified resource from storage.
-	//  *
-	//  * @param  \App\Models\Product  $product
-	//  * @return \Illuminate\Http\Response
-	//  */
-	// public function destroy(Product $product)
-	// {
-	// 	//
-	// }
 }
