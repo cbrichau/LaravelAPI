7542ceee19d99c1fa063d107372c1d1aa1466a72
Clio Brichaut <clio.brichaut@gmail.com>
2022-12-21 19:21:03 +0100

    Cleanup: removing unused code.

--- app/app/Http/Controllers/API/V1/BasketController.php
+++ app/app/Http/Controllers/API/V1/BasketController.php
@@ -175,7 +175,8 @@ class BasketController extends APIController
 			($basket = Basket::find($basketId)) === null ||
 			$basket->user === null ||
 			$basket->user->id !== Auth::id()
-		) {
+		)
+		{
 			return ['NO_BASKET' => "The basket doesn't exist or doesn't belong to the authenticated user."];
 		}
 
@@ -205,81 +206,4 @@ class BasketController extends APIController
 
 		return $errors ?? [];
 	}
-
-
-	// /**
-	//  * Display a listing of the resource.
-	//  *
-	//  * @return \Illuminate\Http\Response
-	//  */
-	// public function index()
-	// {
-	// 	return Basket::all();
-	// }
-
-	// /**
-	//  * Show the form for creating a new resource.
-	//  *
-	//  * @return \Illuminate\Http\Response
-	//  */
-	// public function create()
-	// {
-	// 	//
-	// }
-
-	// /**
-	//  * Store a newly created resource in storage.
-	//  *
-	//  * @param  \App\Http\Requests\StoreBasketRequest  $request
-	//  * @return \Illuminate\Http\Response
-	//  */
-	// public function store(StoreBasketRequest $request)
-	// {
-	// 	//
-	// }
-
-	// /**
-	//  * Display the specified resource.
-	//  *
-	//  * @param  \App\Models\Basket  $basket
-	//  * @return \Illuminate\Http\Response
-	//  */
-	// public function show(Basket $basket)
-	// {
-	// 	//
-	// }
-
-	// /**
-	//  * Show the form for editing the specified resource.
-	//  *
-	//  * @param  \App\Models\Basket  $basket
-	//  * @return \Illuminate\Http\Response
-	//  */
-	// public function edit(Basket $basket)
-	// {
-	// 	//
-	// }
-
-	// /**
-	//  * Update the specified resource in storage.
-	//  *
-	//  * @param  \App\Http\Requests\UpdateBasketRequest  $request
-	//  * @param  \App\Models\Basket  $basket
-	//  * @return \Illuminate\Http\Response
-	//  */
-	// public function update(UpdateBasketRequest $request, Basket $basket)
-	// {
-	// 	//
-	// }
-
-	// /**
-	//  * Remove the specified resource from storage.
-	//  *
-	//  * @param  \App\Models\Basket  $basket
-	//  * @return \Illuminate\Http\Response
-	//  */
-	// public function destroy(Basket $basket)
-	// {
-	// 	//
-	// }
 }
